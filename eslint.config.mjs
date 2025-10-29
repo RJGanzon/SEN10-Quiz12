07dc272c88ba73f3cd3f2194be602eed02cf6d4c
evipuff <ticsaysamantha@gmail.com>
2025-10-29 10:40:03 +0800

    Set up Github Actions workflow with JS Linter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,16 @@
-// eslint.config.js
+// eslint.config.mjs
 import js from "@eslint/js";
 
 export default [
   js.configs.recommended,
   {
+    languageOptions: {
+      globals: {
+        console: "readonly", // allow console.log
+        window: "readonly", // allow window
+        document: "readonly", // allow document
+      },
+    },
     rules: {
       semi: ["error", "always"],
       quotes: ["error", "double"],
