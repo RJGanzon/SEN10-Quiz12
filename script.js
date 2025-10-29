204e38b707ca1c3be7c72ef854d9107ab0dd6cfc
evipuff <ticsaysamantha@gmail.com>
2025-10-29 10:07:36 +0800

    Set up Github Actions workflow with JS Linter

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("This is the JavaScript for the site")
+console.log("This is the JavaScript for the site");
 
 // A function with a deliberate error for testing linter
 function greet() {
