bb0a4bf8e0f8ecf83c845720dce42c06c1a1a192
evipuff <ticsaysamantha@gmail.com>
2025-11-05 10:26:35 +0800

    New activity

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,47 @@
-// eslint.config.mjs
 import js from "@eslint/js";
+import pluginJest from "eslint-plugin-jest";
 
 export default [
   js.configs.recommended,
+
   {
+    files: ["functions.js"],
     languageOptions: {
-      globals: {
-        console: "readonly", // allow console.log
-        window: "readonly", // allow window
-        document: "readonly", // allow document
-      },
+      sourceType: "commonjs",
+    },
+    env: {
+      node: true,
+    },
+  },
+
+  {
+    files: ["functions.test.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+    },
+    env: {
+      node: true,
+      jest: true,
+    },
+    plugins: {
+      jest: pluginJest,
     },
     rules: {
-      semi: ["error", "always"],
-      quotes: ["error", "double"],
+      ...pluginJest.configs.recommended.rules,
+    },
+  },
+
+  {
+    files: ["script.js"],
+    env: {
+      browser: true,
+    },
+  },
+
+  {
+    rules: {
+      "no-undef": "off",
+      "no-unused-vars": "warn",
     },
   },
 ];
